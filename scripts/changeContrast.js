7129869b9f404188a08bd68bac83e733b0080e29
white67 <kamilpudzisz59@gmail.com>
2021-10-10 15:10:16 +0200

    Znowu poprawka kontrastu

--- scripts/changeContrast.js
+++ scripts/changeContrast.js
@@ -2,10 +2,10 @@ let contrastValue = 1
 
 function changeContrast() { 
     if (contrastValue == 1) {
-        $("<style id='contrastFunction'>* { color: #ffff00 !important; background-color: black !important; fill: #ffff00 !important; }</style>").appendTo("head");
+        $("<style id='contrastFunction'>* { color: #ffff00 !important; background-color: black !important; fill: #ffff00 !important; border-color: #ffff00 !important; }</style>").appendTo("head");
         contrastValue = 2;
     } else if (contrastValue == 2){
-        $("<style id='contrastFunction'>* { color: #ffffff !important; background-color: black !important; fill: #ffffff !important; }</style>").appendTo("head");
+        $("<style id='contrastFunction'>* { color: #ffffff !important; background-color: black !important; fill: #ffffff !important; border-color: #ffffff !important;}</style>").appendTo("head");
         (elem=document.getElementById("contrastFunction")).parentNode.removeChild(elem)
         contrastValue = 3;
     } else {
