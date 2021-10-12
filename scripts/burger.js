8c1e3e5bf9ace5e0082fbb81206eb424a12ea417
WinterWollf <wiktor.szyszek894@gmail.com>
2021-10-12 18:29:56 +0200

    Animowany burgerek

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,10 +1,11 @@
-const hamburger = document.querySelector(".burger");
-const navLinks = document.querySelector(".mainNavList");
-const links = document.querySelectorAll(".mainNavList li");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
+const menuBtn = document.querySelector('.icon');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
 });
