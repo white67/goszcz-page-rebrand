b9f5c8f1158c8a8b1891be101914d8b2802aba97
white67 <kamilpudzisz59@gmail.com>
2021-10-14 20:38:42 +0200

    narazie tak

--- scripts/changeContrast.js
+++ scripts/changeContrast.js
@@ -2,20 +2,20 @@ let contrastValue = 1
 
 function changeContrast() { 
     if (contrastValue == 1) {
-        $("<style id='contrastFunction'>* { color: #ffff00 !important; background-color: black !important; fill: #ffff00 !important; border-color: #ffff00 !important; }</style>").appendTo("head");
+        $("<style id='contrastFunction'>* { color: #ffff00 !important; background-color: black !important; fill: #ffff00 !important; border-color: #ffff00 !important; }    .menu-btn__burger { background-color: #ffff00 !important; } .menu-btn__burger::before { background-color: #ffff00 !important; } .menu-btn__burger::after { background-color: #ffff00 !important; }</style>").appendTo("head");
         contrastValue = 2;
 
-        document.documentElement.style.setProperty('--burger', '#ffff00');
+        //document.documentElement.style.setProperty('--burger', '#ffff00');
     } else if (contrastValue == 2){
-        $("<style id='contrastFunction'>* { color: #ffffff !important; background-color: black !important; fill: #ffffff !important; border-color: #ffffff !important;}</style>").appendTo("head");
+        $("<style id='contrastFunction'>* { color: #ffffff !important; background-color: black !important; fill: #ffffff !important; border-color: #ffffff !important;}    .menu-btn__burger { background-color: #ffffff !important; } .menu-btn__burger::before { background-color: #ffffff !important; } .menu-btn__burger::after { background-color: #ffffff !important; }</style>").appendTo("head");
         (elem=document.getElementById("contrastFunction")).parentNode.removeChild(elem)
         contrastValue = 3;
 
-        document.documentElement.style.setProperty('--burger', '#ffffff');
+        //document.documentElement.style.setProperty('--burger', '#ffffff');
     } else {
         (elem=document.getElementById("contrastFunction")).parentNode.removeChild(elem)
         contrastValue = 1;
 
-        document.documentElement.style.setProperty('--burger', '#9a3231');
+        //document.documentElement.style.setProperty('--burger', '#9a3231');
     }
 } 
