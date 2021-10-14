7f7546f6dff4764cb054573588ffd288c08a8310
white67 <kamilpudzisz59@gmail.com>
2021-10-14 20:22:43 +0200

    kontrast burgera

--- scripts/changeContrast.js
+++ scripts/changeContrast.js
@@ -4,12 +4,18 @@ function changeContrast() {
     if (contrastValue == 1) {
         $("<style id='contrastFunction'>* { color: #ffff00 !important; background-color: black !important; fill: #ffff00 !important; border-color: #ffff00 !important; }</style>").appendTo("head");
         contrastValue = 2;
+
+        document.documentElement.style.setProperty('--burger', '#ffff00');
     } else if (contrastValue == 2){
         $("<style id='contrastFunction'>* { color: #ffffff !important; background-color: black !important; fill: #ffffff !important; border-color: #ffffff !important;}</style>").appendTo("head");
         (elem=document.getElementById("contrastFunction")).parentNode.removeChild(elem)
         contrastValue = 3;
+
+        document.documentElement.style.setProperty('--burger', '#ffffff');
     } else {
         (elem=document.getElementById("contrastFunction")).parentNode.removeChild(elem)
         contrastValue = 1;
+
+        document.documentElement.style.setProperty('--burger', '#9a3231');
     }
 } 
