c94243b6627b338c1907b556bbc75f125e58a909
white67 <kamilpudzisz59@gmail.com>
2021-10-15 13:39:11 +0200

    limit powiększania/pomniejszania strony

--- scripts/changeFontSize.js
+++ scripts/changeFontSize.js
@@ -2,15 +2,23 @@ let fontSizeRn = 1
 
 $(document).on('click', '#btnFontSizeNormal', function(){
     $('body').css("font-size", "1rem");
-    fontSizeRn = 1
+    fontSizeRn = 1;
 });
 
 $(document).on('click', '#btnFontSizeDown', function(){
-    $('body').css("font-size", `${fontSizeRn = fontSizeRn - 0.1}rem`);
-    fontSizeRn = fontSizeRn - 0.1
+    if((fontSizeRn > 0.6) && (fontSizeRn < 1.7)) {
+        fontSizeRn = fontSizeRn - 0.1;
+        $('body').css("font-size", `${fontSizeRn}rem`);
+    } else if(fontSizeRn < 0.6) {
+        pass;
+    } 
 });
 
 $(document).on('click', '#btnFontSizeUp', function(){
-    $('body').css("font-size", `${fontSizeRn = fontSizeRn + 0.1}rem`);
-    fontSizeRn = fontSizeRn + 0.1
+    if((fontSizeRn > 0.4) && (fontSizeRn < 1.6)) {
+        fontSizeRn = fontSizeRn + 0.1;
+        $('body').css("font-size", `${fontSizeRn}rem`);
+    } else if(fontSizeRn > 1.5) {
+        pass;
+    } 
 });
