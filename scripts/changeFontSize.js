9b6611d36c13a0481d3e5cf1e35b07e7588e54f1
white67 <kamilpudzisz59@gmail.com>
2021-10-15 14:39:15 +0200

    Dodanie komentarzy

--- scripts/changeFontSize.js
+++ scripts/changeFontSize.js
@@ -1,8 +1,11 @@
-let fontSizeRn = 1
+let fontSizeRn = 1  // ustaw wartość podstawową dla rozmiaru fontu (1 rem)
 
+    // przywrócenie rozmiaru do podstawowego
 $(document).on('click', '#btnFontSizeNormal', function(){
     $('body').css("font-size", "1rem");
     fontSizeRn = 1;
+
+    // jeśli rozmiar fontu jest większy lub równy 1.4, usuń border dla diva o klasie .footerMainLeft
     if(fontSizeRn >= 1.4){
         $('.footerMainLeft').css("border-right", "none");
     } else if(fontSizeRn < 1.4){
@@ -10,10 +13,13 @@ $(document).on('click', '#btnFontSizeNormal', function(){
     }
 });
 
+    // zmniejszanie rozmiaru fontu -0.1rem
 $(document).on('click', '#btnFontSizeDown', function(){
     if((fontSizeRn > 0.6) && (fontSizeRn < 1.7)) {
-        fontSizeRn = fontSizeRn - 0.1;
+        fontSizeRn = fontSizeRn - 0.1;  // zaktualizuj wartość dla obecnego rozmiaru
         $('body').css("font-size", `${fontSizeRn}rem`);
+
+        // jeśli rozmiar fontu jest większy lub równy 1.4, usuń border dla diva o klasie .footerMainLeft
         if(fontSizeRn >= 1.4){
             $('.footerMainLeft').css("border-right", "none");
         } else if(fontSizeRn < 1.4){
@@ -21,19 +27,22 @@ $(document).on('click', '#btnFontSizeDown', function(){
         }
     } else if(fontSizeRn < 0.6) {
         pass;
-    } 
+    }   // ustawienie limitu pomniejszenia rozmiaru fontu
 });
 
+    // zwięszkanie rozmiaru fontu +0.1rem
 $(document).on('click', '#btnFontSizeUp', function(){
     if((fontSizeRn > 0.4) && (fontSizeRn < 1.6)) {
-        fontSizeRn = fontSizeRn + 0.1;
+        fontSizeRn = fontSizeRn + 0.1;  // zaktualizuj wartość dla obecnego rozmiaru
         $('body').css("font-size", `${fontSizeRn}rem`);
+
+        // jeśli rozmiar fontu jest większy lub równy 1.4, usuń border dla diva o klasie .footerMainLeft
         if(fontSizeRn >= 1.4){
             $('.footerMainLeft').css("border-right", "none");
         } else if(fontSizeRn < 1.4){
             $('.footerMainLeft').css("border-right", "");
         }
-    } else if(fontSizeRn > 1.5) {
+    } else if(fontSizeRn > 1.6) {
         pass;
-    } 
+    }   // ustawienie limitu powiększenia rozmiaru fontu
 });
