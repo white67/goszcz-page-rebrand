3cf8dc9643d3070e3ca5ea23b751ccda641cd144
white67 <kamilpudzisz59@gmail.com>
2021-10-15 13:56:18 +0200

    poprawka przy zwiększaniu rozmiaru fontu

--- scripts/changeFontSize.js
+++ scripts/changeFontSize.js
@@ -3,12 +3,22 @@ let fontSizeRn = 1
 $(document).on('click', '#btnFontSizeNormal', function(){
     $('body').css("font-size", "1rem");
     fontSizeRn = 1;
+    if(fontSizeRn >= 1.4){
+        $('.footerMainLeft').css("border-right", "none");
+    } else if(fontSizeRn < 1.4){
+        $('.footerMainLeft').css("border-right", "");
+    }
 });
 
 $(document).on('click', '#btnFontSizeDown', function(){
     if((fontSizeRn > 0.6) && (fontSizeRn < 1.7)) {
         fontSizeRn = fontSizeRn - 0.1;
         $('body').css("font-size", `${fontSizeRn}rem`);
+        if(fontSizeRn >= 1.4){
+            $('.footerMainLeft').css("border-right", "none");
+        } else if(fontSizeRn < 1.4){
+            $('.footerMainLeft').css("border-right", "");
+        }
     } else if(fontSizeRn < 0.6) {
         pass;
     } 
@@ -18,6 +28,11 @@ $(document).on('click', '#btnFontSizeUp', function(){
     if((fontSizeRn > 0.4) && (fontSizeRn < 1.6)) {
         fontSizeRn = fontSizeRn + 0.1;
         $('body').css("font-size", `${fontSizeRn}rem`);
+        if(fontSizeRn >= 1.4){
+            $('.footerMainLeft').css("border-right", "none");
+        } else if(fontSizeRn < 1.4){
+            $('.footerMainLeft').css("border-right", "");
+        }
     } else if(fontSizeRn > 1.5) {
         pass;
     } 
